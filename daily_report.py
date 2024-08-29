7da895dc10263dd7db2ffb2d1d982d750294879d
DaoXuan <116349858+dx2331lxz@users.noreply.github.com>
2024-08-29 10:17:14 +0800

    Create daily_report.py

---
+++ daily_report.py
@@ -0,0 +1,35 @@
+import json
+
+import requests
+
+url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send"
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+params = {
+    # "key": "c4dbad8b-2fec-498e-b69a-a45f394eaf27"
+    "key": "6157cdf4-08b7-4513-a30c-2ed9378f1c2f"
+}
+
+data = {
+    "msgtype": "text",
+    "text": {
+        "content": "别忘记提交今天的日报",
+        "mentioned_list": ["@all"],
+    }
+}
+
+response = requests.post(url, headers=headers, params=params, json=data)
+if response.status_code == 200:
+    print("发送成功")
+else:
+    # 记录日志
+    with open("log.txt", "a") as f:
+        f.write(json.dumps(response.json()) + "\n")
+
+    print("发送失败")
+    print(response.json())
+
+
